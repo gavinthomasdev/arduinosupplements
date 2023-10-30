1be88b88e6132d9d662d24d88fc489d58606cb1f
Gavin Thomas <gavin@gavinthomas.dev>
2023-10-30 11:49:38 -0400

    Create Code4.ino

---
+++ Code4.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo servo1;
+
+int pos = 0;
+
+void setup() {
+  servo1.attach(9);
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) {
+    servo1.write(pos);
+    delay(20);
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    servo1.write(pos);
+    delay(20);
+  }
+}
