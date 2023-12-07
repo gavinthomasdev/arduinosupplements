8f4bff9280eb4a76f65a1dcf2f4d0dd9ba3b5964
Gavin Thomas <gavin@gavinthomas.dev>
2023-12-07 12:50:50 -0500

    Update Code3Updated.ino

--- Code3Updated/Code3Updated.ino
+++ Code3Updated/Code3Updated.ino
@@ -1,20 +1,20 @@
-const int LED1 = 8, BUTTONON = 4, BUTTONOFF = 5;
+const int LED1 = 8, BUTTONON = 4, BUTTONOFF = 5; // pins
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println(F("Program Begin"));
+  Serial.begin(9600); // init serial port at 9600 baud
+  Serial.println(F("Program Begin")); // print string "Program Begin" from flash
 
-  pinMode(LED1, OUTPUT);
-  pinMode(BUTTONON, INPUT);
-  pinMode(BUTTONOFF, INPUT);
+  pinMode(LED1, OUTPUT); // set LED1 pin to output
+  pinMode(BUTTONON, INPUT); // set BUTTONON pin to input
+  pinMode(BUTTONOFF, INPUT); // set BUTTONOFF pin to input
 }
 
 void loop() {
-  bool ledOn = digitalRead(LED1);
-  int toggle = (ledOn ? digitalRead(BUTTONOFF) : digitalRead(BUTTONON));
+  bool ledOn = digitalRead(LED1); // store current digital value of LED1 pin
+  int toggle = (ledOn ? digitalRead(BUTTONOFF) : digitalRead(BUTTONON)); // (ternary) if ledOn then read digital value from BUTTONOFF. else, read digital value from BUTTONON
   
-  if (toggle) {
-    digitalWrite(LED1, !ledOn);
-    delay(20);
+  if (toggle) { // if toggle
+    digitalWrite(LED1, !ledOn); // write opposite digital value to LED1 pin
+    delay(20); // delay for 20 ms
   }
 }
